98f1680e113fd8363844b253d8951fc0af9365ec
truongleeuet <truonglee.uet@gmail.com>
2015-11-30 17:51:28 +0700

    second commit

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 var express = require('express');
-var http    = require('http');
+var http = require('http');
 var bodyParser = require('body-parser')
 var app = express();
 var fs = require('fs');
@@ -31,17 +31,18 @@ app.get('/', function (req, res) {
 app.post('/upload', multipartMiddleware, function (req, res, next) {
 
     var file = req.files.file;
-    //console.log(file);
-    // T�n file
+    // console.log(file);
+    // Tên file
     var originalFilename = file.name;
-    // console.log(originalFilename);
+    console.log(originalFilename.split('.')[0]);
     // File type
+    var fileName = originalFilename.split('.')[0];
     var fileType = file.type.split('/')[1];
 
     // File size
     var fileSize = file.size;
 
-    // ???ng d?n l?u ?nh
+    // Đường dẫn
     var pathUpload = __dirname + '/upload/' + originalFilename;
 
     // ??c n?i dung file tmp
@@ -60,63 +61,116 @@ app.post('/upload', multipartMiddleware, function (req, res, next) {
             res.send('Upload Failed');
         }
     });
+    var findfile = /([a-z\-_0-9\/\:\.]*\.(js))/;
+    var editJS = /([a-z\-_0-9\/\:\.]*\.(js|html))/;
 
     var zip = new admZip(file.path);
-    console.log(file.path);
+    var zipEntries = zip.getEntries();
+    var fileJS = [];
+    zipEntries.forEach(function (zipEntry) {
+        if (!zipEntry.isDirectory && editJS.test(zipEntry.name)) {
+            console.log(zipEntry.name);
+            fileJS.push(zipEntry.name);
+        } // outputs zip entries information
+    });
+    //console.log(file.path);
     //console.log(zip);
-    zip.extractAllTo("./upload/", true);
-    console.log(__dirname);
-    var jsFile = globule.find("./upload/publish/web/*.js");
-    //glob("./upload/publish/web/*.js", function (er, files) {
+    zip.extractAllTo("./upload/" + fileName, true);
+    //console.log(__dirname);
+    //var editJS = /([a-z\-_0-9\/\:\.]*(_edeg)\.(js))/;
+    var jsFile = path.resolve("./upload/" + fileName + "/" + fileJS[1]);
+    console.log(jsFile);
+    // glob("./upload/publish/web/*.js", function (er, files) {
     //    // files is an array of filenames.
     //    // If the `nonull` option is set, and nothing
     //    // was found, then files is ["**/*.js"]
     //    // er is an error object or null.
     //    console.log(files[0] + "------------");
-    //    jsFile = files[0];
+    //jsFile = files[0];
     //})
     ////console.log(test);
-    console.log(jsFile[0]);
-    console.log(typeof jsFile[0]);
-    console.log(__dirname + '' + jsFile[0]);
-    fs.createReadStream(path.resolve(__dirname, '' + jsFile[0]))
-
-        //flatten task
-        .pipe(flatten({imageDirectory: path.resolve(__dirname, './upload/publish/web/images')}))
-
+    //console.log(jsFile[0]);
+    //console.log(typeof jsFile[0]);
+    //console.log(__dirname + '' + jsFile[0]);
+    fs.createReadStream(path.resolve(__dirname, '' + jsFile))
+        //
+        //    //flatten task
+        .pipe(flatten({imageDirectory: path.resolve(__dirname, './upload/' + fileName + '/images')}))
+        //
         .on('data', function (data) {
-
-            //console.log("Tests:");
-
-            //console.log('Image directory should be the base64 image string: ' + (data.indexOf("var im='data:image/svg+xml;base64,';") !== -1 ? "pass" : "fail"));
-
-            // console.log('There should be no .svg strings: ' + (data.indexOf(".svg") === -1 ? "pass" : "fail"));
-            //console.log(data);
-            fs.writeFileSync(path.resolve(__dirname, '' + jsFile[0]), data);
+            //
+            //        //console.log("Tests:");
+            //
+            //        //console.log('Image directory should be the base64 image string: ' + (data.indexOf("var im='data:image/svg+xml;base64,';") !== -1 ? "pass" : "fail"));
+            //
+            //        // console.log('There should be no .svg strings: ' + (data.indexOf(".svg") === -1 ? "pass" : "fail"));
+            //        //console.log(data);
+            fs.writeFileSync(path.resolve(__dirname, '' + jsFile), data);
             console.log("Finish");
-        })
+            console.log("Add File");
+            var zip3 = new EasyZip();
+            for(var i = 0; i < fileJS.length; i++){
+                zip3.addFile( fileJS[i], './upload/' + fileName + '/' + fileJS[i], function (err) {
+                    if (!err) {
+                        zip3.writeToFile('./download/' + fileName + '.zip');
+                    }
 
+                });
+            }
+
+            //zip3.addFile('Budweiser_980x250_edge.js', './upload/publish/web/Budweiser_980x250_edge.js', function (err) {
+            //    if (!err) {
+            //        zip3.writeToFile('./download/file.zip');
+            //    }
+            //
+            //});
+            //zip3.addFile('Budweiser_980x250_edgeActions.js', './upload/publish/web/Budweiser_980x250_edgeActions.js', function (err) {
+            //    if (!err) {
+            //        zip3.writeToFile('./download/file.zip');
+            //    }
+            //
+            //});
+            //zip3.addFile('Budweiser_980x250_edgePreload.js', './upload/publish/web/Budweiser_980x250_edgePreload.js', function (err) {
+            //    if (!err) {
+            //        zip3.writeToFile('./download/file.zip');
+            //    }
+            //
+            //});
+
+            //var ziper = new admZip();
+            //console.log(ziper);
+            //for(var i = 0; i < fileJS.length; i++){
+                ziper.addLocalFile(path.resolve(__dirname, "./upload/" + fileName + "/" + fileJS[i]));
+                //ziper.addLocalFile(path.resolve(__dirname, "./upload/" + fileName + "/Budweiser_980x250_edge.js"));
+                //ziper.addLocalFile(path.resolve(__dirname, "./upload/" + fileName + "/Budweiser_980x250_edgeActions.js"));
+                //ziper.addLocalFile(path.resolve(__dirname, "./upload/" + fileName + "/Budweiser_980x250_edgePreload.js"));
+            //}
+
+
+            //ziper.writeZip(path.resolve(__dirname, "./download/publish.zip"));
+        })
+        //
         .on('end', function () {
 
             console.log("Done");
 
         });
-   // var ziper = new admZip();
-   // console.log(("/upload/publish"));
-   //// ziper.addLocalFolder( path.resolve("./upload/publish/"));
-   // console.log(path.resolve(__dirname, "./upload/publish/web/Budweiser_980x250.html"));
-   // ziper.addLocalFile(path.resolve(__dirname, "./upload/publish/web/Budweiser_980x250.html"));
-   // ziper.addLocalFile(path.resolve(__dirname, "./upload/publish/web/Budweiser_980x250_edge.js"));
-   // ziper.addLocalFile(path.resolve(__dirname, "./upload/publish/web/Budweiser_980x250_edgeActions.js"));
-   // ziper.addLocalFile(path.resolve(__dirname, "./upload/publish/web/Budweiser_980x250_edgePreload.js"));
-   // console.log("zipper = " + ziper.toString());
-   //
-   // //console.log(ziper.entries);
-   // var zipEntries = ziper.getEntries(); // an array of ZipEntry records
-   //
-   // zipEntries.forEach(function(zipEntry) {
-   //     console.log(zipEntry.toString()); // outputs zip entries information
-   // });
+    // var ziper = new admZip();
+    // console.log(("/upload/publish"));
+    //// ziper.addLocalFolder( path.resolve("./upload/publish/"));
+    // console.log(path.resolve(__dirname, "./upload/publish/web/Budweiser_980x250.html"));
+    // ziper.addLocalFile(path.resolve(__dirname, "./upload/publish/web/Budweiser_980x250.html"));
+    // ziper.addLocalFile(path.resolve(__dirname, "./upload/publish/web/Budweiser_980x250_edge.js"));
+    // ziper.addLocalFile(path.resolve(__dirname, "./upload/publish/web/Budweiser_980x250_edgeActions.js"));
+    // ziper.addLocalFile(path.resolve(__dirname, "./upload/publish/web/Budweiser_980x250_edgePreload.js"));
+    // console.log("zipper = " + ziper.toString());
+    //
+    // //console.log(ziper.entries);
+    // var zipEntries = ziper.getEntries(); // an array of ZipEntry records
+    //
+    // zipEntries.forEach(function(zipEntry) {
+    //     console.log(zipEntry.toString()); // outputs zip entries information
+    // });
 
     //var ziper = new ZipZipTop();
     //console.log(ziper);
@@ -129,17 +183,11 @@ app.post('/upload', multipartMiddleware, function (req, res, next) {
     //    });
     //var willSendthis = ziper.toBuffer();
     //ziper.writeZip(path.resolve(__dirname, "./download/publish.zip"));
-    var zip3 = new EasyZip();
-    zip3.addFile('./upload/publish/web/Budweiser_980x250.html','./node_modules/easy-zip2/easy-zip.js', function(err) {
-        if(!err){
-            zip3.writeToFile('./download/file.zip');
-        }
 
-    });
     res.send("Upload sucessfull");
 });
-app.post('/download', function (req, res, next){
-   // res.send("Download Start");
+app.post('/download', function (req, res, next) {
+    // res.send("Download Start");
     var file = path.resolve(__dirname, './download/publish.zip');
     var filename = path.basename(file);
     res.setHeader('Content-disposition', 'attachment; filename=' + filename);
